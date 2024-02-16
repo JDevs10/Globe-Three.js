8db09132773ed5d2373e2a7b15e244d904db8671
JDevs10 <j-ldsxm18@hotmail.com>
2024-02-16 13:37:06 +0100

    add stars

--- src/index.ts
+++ src/index.ts
@@ -62,6 +62,26 @@ group.add(sphere)
 group.add(atmosphere)
 scene.add(group)
 
+
+// create stars
+const starGeometry = new THREE.BufferGeometry();
+const starMaterial = new THREE.PointsMaterial({
+    color: 0xffffff
+});
+
+const starVertices = [];
+for (let i = 0; i < 10000; i++) {
+    const x = (Math.random() - 0.5) * 2000;
+    const y = (Math.random() - 0.5) * 2000;
+    const z = -Math.random() * 1000;
+    starVertices.push(x, y, z);
+}
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3));
+
+const stars = new THREE.Points(starGeometry, starMaterial);
+scene.add(stars);
+
+
 camera.position.z = 15;
 
 const mouse: any = {
